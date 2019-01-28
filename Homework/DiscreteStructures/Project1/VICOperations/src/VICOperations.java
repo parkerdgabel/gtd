85f0a5131f828609062e3da93b1b487ee2639482
Parker Gabel <parkerdgabel@email.arizona.edu>
2019-01-27 18:07:06 -0700

    VICoperations is done

--- Homework/DiscreteStructures/Project1/VICOperations/src/VICOperations.java
+++ Homework/DiscreteStructures/Project1/VICOperations/src/VICOperations.java
@@ -104,12 +104,110 @@ public class VICOperations {
         }
     }
 
+    /** Converts a string into a set of chars
+     * @param anagram       The string to convert
+     * @return              a set of characters
+     */
+    private static Set<Character> _convertStringToCharacterSet(String anagram) {
+       Set<Character> anaChars = new HashSet<>();
+       for (char ch : anagram.toCharArray()) {
+           anaChars.add(ch);
+       }
+       return anaChars;
+    }
+
+    /** Generates the complement of the anagram
+     * @param anagram       The anagram to take the complement of
+     * @return              A list of characters representing the complement
+     */
+    private static List<Character> _generateRestOfAlphabet(String anagram) {
+        List<Character> lst = new ArrayList<>();
+        Set<Character> anaChars = _convertStringToCharacterSet(anagram);
+        for (int ch = 'A'; ch <= 'Z'; ch++) {
+            if (!anaChars.contains((char) ch)) {
+                lst.add((char) ch);
+            }
+        }
+        return lst;
+    }
+
+    /** Splits the remaining letters into two list of appropriate size
+     * @param rest      The rest of the alphabet
+     * @return          A list of lists of characters.
+     */
+    private static List<List<Character>> _splitRestOfAlphabet(List<Character> rest) {
+        if (rest.size() < 10) {
+            throw new IllegalArgumentException("Rest of alphabet must be greater that 10");
+        }
+        List<List<Character>> lst = new ArrayList<List<Character>>();
+        lst.add(rest.subList(0, 10));
+        lst.add(rest.subList(10, rest.size()));
+        return lst;
+    }
+
+    /** Maps the characters of the anagram to the permutation.
+     * @param permutation       the permutation for keys
+     * @param anagram           the anagram to associate
+     * @param alphaMap          the main map for the straddling checkerboard
+     */
+    private static void _mapAnagramToPermutation(String permutation, String anagram, Map<Character, String> alphaMap) {
+        for (int i = 0; i < anagram.length(); i++) {
+            if (anagram.charAt(i) != ' ') {
+                alphaMap.put(anagram.charAt(i), Character.toString(permutation.charAt(i)));
+            }
+        }
+    }
+
+    /** Associates the whitespace of the anagram with the rest of the alphabet
+     * @param permutation       the permutation for keys
+     * @param anagram           the anagram to associate
+     * @return                  a map with the associations
+     */
+    private static Map<Character, List<Character>> _mapWhitespaceToRestOfAlphabet(String permutation, String anagram) {
+        HashMap<Character, List<Character>> map = new HashMap<>();
+        char firstSpace = permutation.charAt(anagram.indexOf(" "));
+        char lastSpace = permutation.charAt(anagram.lastIndexOf(" "));
+        List<List<Character>> lst = _splitRestOfAlphabet(_generateRestOfAlphabet(anagram));
+        map.put(firstSpace, lst.get(0));
+        map.put(lastSpace, lst.get(1));
+        return map;
+    }
+
+    /** Associates the rest of the alphabet with their respective strings.
+     * @param permutation
+     * @param whiteSpaceMap
+     * @param alphaMap
+     */
+    private static void _assocAlphaMapWithRestOfAlphabet(String permutation, Map<Character, List<Character>> whiteSpaceMap, Map<Character, String> alphaMap) {
+        for (char ch : whiteSpaceMap.keySet()) {
+            for (int i = 0; i < whiteSpaceMap.get(ch).size(); i++) {
+                alphaMap.put(whiteSpaceMap.get(ch).get(i), Character.toString(ch) + Character.toString(permutation.charAt(i)));
+            }
+        }
+    }
+
+    /** Generates the final List for the straddling checkerboard
+     * @param alphaMap      The map of the alphabet to strings
+     * @return              The final list of the straddling checkerboard
+     */
+    private static List<String> _generateStraddlingCheckerboard(TreeMap<Character, String> alphaMap) {
+        List<String> checkerBoard = new ArrayList<>();
+        for (char ch: alphaMap.keySet()) {
+            checkerBoard.add(alphaMap.get(ch));
+        }
+        return checkerBoard;
+    }
+
     /** Associates letters to numbers.
      * @param permutation       The given permutation
      * @param anagram           the anagram to associate
      * @return                  A list of Strings
      */
     public static List<String> straddlingCheckerboard(String permutation, String anagram) {
-        return null;
+        TreeMap<Character, String> alphaMap = new TreeMap<>();
+        String nAnagram = anagram.toUpperCase();
+        _mapAnagramToPermutation(permutation, nAnagram, alphaMap);
+        _assocAlphaMapWithRestOfAlphabet(permutation, _mapWhitespaceToRestOfAlphabet(permutation, anagram), alphaMap);
+        return _generateStraddlingCheckerboard(alphaMap);
     }
 }
